9bed2ce33ed4d6da4bd1751e377beeb2e4b99782
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-01-16 01:09:28 -0300

    Exercício 3_1 é a resolucao final do exercício, ele abrange a possibilidade do autor ter vários nomes.

--- aula2/exercicio3_1.js
+++ aula2/exercicio3_1.js
@@ -10,7 +10,7 @@ const input = require('../input');
         let author = await input()//vai esperar o usuário digitar algo e vai armazenar na variável author   
         const arrayAuthor = author.split(" ")//vai separar o nome do autor em um array a partir do espaço
         
-        console.log(`${arrayAuthor[arrayAuthor.length - 1].toUpperCase()}, ${author.charAt(0).toUpperCase()}${arrayAuthor[0].slice(1).toLowerCase()} escreveu "${poema}"`)
+        console.log(`${arrayAuthor[1].toUpperCase()}, ${author.charAt(0).toUpperCase()}${arrayAuthor[0].slice(1).toLowerCase()} escreveu ${poema}`)
 
         //${arrayAuthor[1].toUpperCase()} vai pegar o sobrenome do autor e transformar em maiúsculo
         //${author.charAt(0).toUpperCase()} vai pegar a primeira letra do nome do autor e transformar em maiúsculo
