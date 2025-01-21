cc218e0da404c65aac314f5b5c3d038ca100bf45
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-01-21 19:54:31 -0300

    primeiro exemplo com loop

--- aula4/exemplo1.js
+++ aula4/exemplo1.js
@@ -3,8 +3,6 @@ const input = require('../input');
 /* () => {} isso é uma função anônima*/
 (
     async () => {
-        console.log("\nEscreva um verso de poema para mim!")
-        let poema = await input()
         
         console.log("\nQual o nome do autor?")
         let author = await input()
