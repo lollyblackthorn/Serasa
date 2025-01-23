9b93a31047f56c7e15f66a4358c2342287a9f0be
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-01-23 19:11:06 -0300

    Cálculo de fatorial usando laço FOR

---
+++ aula4/exercicio4.js
@@ -0,0 +1,29 @@
+// peça um núero ao usuário e exiba o fatorial
+//n! = n*(n-1)*(n-2)
+
+const input = require('../input');
+
+(
+    async () => {
+
+        console.log("Digite um número: ")   
+        let numberIniti = await input()
+        let num = numberIniti
+
+
+        //let resultFatorial = num
+        
+        if( num == 0 || num == 1){
+            console.log("\nFatorial = 1")
+        }else{
+
+            for(i = num - 1; i >= 1; i--){
+                num = num*i
+            }
+            console.log(`O fatorial de ${numberIniti} é igual a ${num}`)
+
+        }        
+
+
+    }
+)()
