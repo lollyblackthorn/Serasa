460ed0124c2cf639689c6f05505a78637a1e0640
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-02-18 22:18:34 -0300

    criando obj via registro

--- secretaria/documents.js
+++ secretaria/documents.js
@@ -2,13 +2,39 @@
 /*documentos - prontuário/dadosalunos, informações de ocorrencias, histórico, 
 declaração de matricula, boletim, diploma, atestado, RELATÓRIO - */
 
+var documents = []
 
-
-function setDocuments(req,res){
+function setDocuments(req){
     let d = {//lista, registro, objAnonimo
-        id: req.id
+        id: req.id,
+        descripition: req.descripition,
+        url: req.url,                       //caso o usuário tenha q enviar algum arquivo/midia, pegar caminho de um arquivo
+        createAt: req.createAt,             //data que foi criado
+        userId: req.userId,
+        type: req.type,                     //tipo de documento boletim, diploma, atestado, etc.
+
 
     }
 
+    documents.push(d)                       //empurra o obj d na lista documents
 }
 
+setDocuments({
+    id: 0,
+    descripition: "Esse é meu primeiro post",
+    url: "http://caminhodomeuarquivo.com",    //caso o usuário tenha q enviar algum arquivo/midia, pegar caminho de um arquivo
+    createAt: "2000-01-31",                  //data que foi criado
+    userId: 0,
+    //type: req.type,                         //tipo de documento boletim, diploma, atestado, etc.
+})
+
+setDocuments({
+    id: 1,
+    descripition: "Esse é meu primeiro post",
+    url: "http://caminhodomeuarquivo.com",    //caso o usuário tenha q enviar algum arquivo/midia, pegar caminho de um arquivo
+    createAt: "2000-01-31",                  //data que foi criado
+    userId: 0,
+    //type: req.type,                         //tipo de documento boletim, diploma, atestado, etc.
+})
+
+console.log(documents)
