850348428a73ef515353695a6f238dc111e532fd
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-02-18 21:16:53 -0300

    atualização dos objetos e dos responsáveis

---
+++ secretaria/documents.js
@@ -0,0 +1,15 @@
+//Atributos = campos de um formulário
+/*documentos - prontuário/dadosalunos, informações de ocorrencias, histórico, 
+declaração de matricula, boletim, diploma, atestado, RELATÓRIO - */
+
+//criar objetos
+var studentRecord = new Object();
+studentRecord.id = 
+studentRecord.nameStudent = 
+studentRecord.class = " ";
+
+var ocorrencias = new Object();
+ocorrencias.id = 
+ocorrencias 
+
+
