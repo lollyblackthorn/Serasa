9733e3fb6d072245c5294635397aade764f6d098
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-02-18 22:17:51 -0300

    criando os obj via registro

--- secretaria/documents.js
+++ secretaria/documents.js
@@ -2,15 +2,7 @@
 /*documentos - prontuário/dadosalunos, informações de ocorrencias, histórico, 
 declaração de matricula, boletim, diploma, atestado, RELATÓRIO - */
 
-//criar objetos
-var studentRecord = new Object();
-studentRecord.id = 
-studentRecord.nameStudent = 
-studentRecord.class = " ";
 
-var ocorrencias = new Object();
-ocorrencias.id = 
-ocorrencias 
 
 function setDocuments(req,res){
     let d = {//lista, registro, objAnonimo
